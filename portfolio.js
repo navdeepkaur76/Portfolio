8b7bf6b5ee6138be90ecec3787e4170501524558
navdeepkaur76 <navdeepkaurr076@gmail.com>
2025-06-29 18:11:07 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+
+  document.getElementById("btn").addEventListener("click", function() {
+    window.open("https://github.com/navdeepkaur76", "_blank"); 
+  });
+
+  document.getElementById("res").addEventListener("click", function() {
+    window.open("file:///D:/portfolio/Resume.pdf", "_blank"); 
+  });
